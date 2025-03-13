df40c4fdb5a69f6c665c7989ddad7eeb622f617e
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-13 13:48:35 -0400

    add tables to migrations folder

---
+++ migrations/20250313165848_create_venue_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable('venues', (table) => {
+      table.increments('id').primary(); 
+      table.string('name').notNullable(); 
+      table.string('neighbourhood').notNullable(); 
+      table.string('address').notNullable();  
+      table.string('website').nullable(); 
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));  // Timestamp for updates
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  export function down(knex) {
+    return knex.schema.dropTableIfExists('venues'); 
+  };
