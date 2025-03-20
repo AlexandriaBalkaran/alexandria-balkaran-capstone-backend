c311d59ddaec180296913453293b83aa62a910e0
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-03-20 12:55:17 -0400

    add lat and lng

--- migrations/20250313165848_create_venue_table.js
+++ migrations/20250313165848_create_venue_table.js
@@ -3,22 +3,24 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-    return knex.schema.createTable('venues', (table) => {
-      table.increments('id').primary(); 
-      table.string('name').notNullable(); 
-      table.string('neighbourhood').notNullable(); 
-      table.string('address').notNullable();  
-      table.string('photo').notNullable(); 
-      table.string('website').nullable(); 
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));  // Timestamp for updates
-    });
-  };
-  
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Promise<void> }
-   */
-  export function down(knex) {
-    return knex.schema.dropTableIfExists('venues'); 
-  };
+  return knex.schema.createTable('venues', (table) => {
+    table.increments('id').primary(); 
+    table.string('name').notNullable(); 
+    table.string('neighbourhood').notNullable(); 
+    table.string('address').notNullable();  
+    table.string('photo').notNullable(); 
+    table.string('website').nullable(); 
+    table.decimal('lat', 9, 6).notNullable();
+    table.decimal('lng', 9, 6).notNullable(); 
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+  });
+};
+
+/**
+* @param { import("knex").Knex } knex
+* @returns { Promise<void> }
+*/
+export function down(knex) {
+  return knex.schema.dropTableIfExists('venues'); 
+};
